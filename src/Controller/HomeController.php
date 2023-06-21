2204a1c7c41dd3805d2ce39cc45d0bd1121f7f16
Takashi Kanemoto <ttskch@gmail.com>
2023-06-21 11:26:18 +0900

    fix: 🐛 fix N+1 query problem by joining

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +21,12 @@ class HomeController extends AbstractController
             ->add('user', EntityType::class, [
                 'class' => User::class,
                 'placeholder' => 'Please select',
+                'query_builder' => function (UserRepository $repository) {
+                    return $repository->createQueryBuilder('u')
+                        ->leftJoin('u.team', 't')
+                        ->addSelect('t')
+                    ;
+                },
             ])
             ->getForm()
         ;
