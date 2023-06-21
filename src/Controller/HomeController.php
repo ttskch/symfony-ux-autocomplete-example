e70cf10a8b1c0d7930cb57a80ba97b5b0f24a5b0
Takashi Kanemoto <ttskch@gmail.com>
2023-06-21 11:23:32 +0900

    feat: 🎸 impl an example form **(with N+1 query problem)**

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'home_')]
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'placeholder' => 'Please select',
+            ])
+            ->getForm()
+        ;
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
